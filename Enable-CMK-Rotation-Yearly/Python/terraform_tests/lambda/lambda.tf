7b08b74db7dbb3bd77a4a463b20fee4ea2ac5ca7
Anon4Now <tutkoda@gmail.com>
2023-02-26 13:13:20 -0500

    updated terraform module to look for zip file in same dir

--- Enable-CMK-Rotation-Yearly/Python/terraform_tests/lambda/lambda.tf
+++ Enable-CMK-Rotation-Yearly/Python/terraform_tests/lambda/lambda.tf
@@ -8,9 +8,9 @@ variable "lambda_role_arn" {
 
 resource "aws_lambda_function" "versioning_lambda" {
   function_name = var.lambda_func_name
-  filename = "C:\\terraform_projects\\s3-enable-versioning-infra\\lambda\\main.zip"
+  filename = "./main.zip"
   role = var.lambda_role_arn
-  handler = "main"
-  runtime = "go1.x"
+  handler = "cmk_rotate_keys_one_year"
+  runtime = "python3.9"
 
 }
