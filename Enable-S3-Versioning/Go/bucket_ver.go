6d9e803e7647b80e91b198cd21b85a90beb2a3d3
Anon4Now <tutkoda@gmail.com>
2023-03-11 18:57:49 -0500

    updated bucket file with code for testing

--- Enable-S3-Versioning/Go/bucket_ver.go
+++ Enable-S3-Versioning/Go/bucket_ver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -11,11 +12,19 @@ import (
 
 
 type Bucket struct {
+	/*
+	Struct that contains the attrs and methods to set S3 versioning on buckets.
+	*/
 	Client S3ActionsApi
 	BucketList []string
 }
 
-func (b *Bucket) bucketList() {	
+func (b *Bucket) bucketList() {
+	/*
+	Private method that checks what buckets are available to the role in the AWS account.
+
+	:return: nil
+	*/	
 	resp, err := b.Client.ListBuckets(context.TODO(), nil)
 
 	if err != nil {
@@ -30,7 +39,15 @@ func (b *Bucket) bucketList() {
 
 }
 
-func (b *Bucket) updateBucketVersion(bucket string, message string) {
+func (b *Bucket) updateBucketVersion(bucket string) bool {
+	/*
+	Private method to set versioning on a specific S3 bucket within the AWS account.
+
+	Will return 'true' if the method runs successfully.
+
+	:param bucket: (required) A string containing the S3 bucket name
+	:return: A boolean result when run successfully
+	*/
 	params := &s3.PutBucketVersioningInput {
 		Bucket: &bucket,
 		VersioningConfiguration: &types.VersioningConfiguration {
@@ -43,9 +60,17 @@ func (b *Bucket) updateBucketVersion(bucket string, message string) {
 	if err != nil {
 		log.Println(err)
 	}
+
+	return true
 }
 
 func (b *Bucket) getBucketVersion(bucket string) (*s3.GetBucketVersioningOutput, error) {
+	/*
+	Private method that will get the current status of versioning on an S3 bucket.
+
+	:param bucket: (required) A string containing the name of the S3 bucket
+	:return: Will return either a struct output or an error from AWS
+	*/
 
 	params := &s3.GetBucketVersioningInput {
 		Bucket: &bucket,
@@ -54,6 +79,13 @@ func (b *Bucket) getBucketVersion(bucket string) (*s3.GetBucketVersioningOutput,
 }
 
 func (b *Bucket) checkBucketVersion() map[string]string{
+	/*
+	Private method that will make a map key with the bucket version status.
+
+	The map key will be concatenated with the index value of the struct slice.
+
+	:return: A map with a string key and string value (i.e., {"enabled0": "bucketName", "suspended1": "bucketnane1"})
+	*/
 	m := make(map[string]string)
 	for i, bucket := range b.BucketList {
 		resp, err := b.getBucketVersion(bucket)
@@ -61,19 +93,26 @@ func (b *Bucket) checkBucketVersion() map[string]string{
 		if err != nil {
 			log.Println(err)
 		}
-
+			// need to perform a conversion on the index number to avoid a testing error
 			if resp.Status == "Suspended" {
-				m["suspended" + string(i)] = bucket
+				m["suspended" + strconv.FormatInt(int64(i), 10)] = bucket
 			} else if resp.Status == "Enabled"{
-				m["enabled" + string(i)] = bucket
+				m["enabled" + strconv.FormatInt(int64(i), 10)] = bucket
 			} else {
-				m["disabled" + string(i)] = bucket
+				m["disabled" + strconv.FormatInt(int64(i), 10)] = bucket
 			}
 	}
 	return m
 }
 
 func (b *Bucket) removeItemFromDict(mapKey string, bucketMap map[string]string) bool {
+	/*
+	Private method that will remove items from the shallow copy of the map.
+
+	:param mapKey: (required) The string key that will point to the val in the map to remove
+	:param bucketMap: (required) A map with a string key and a string value for bucket name and status
+	:return: A boolean result that depends on whether the map deletion was successful
+	*/
 	if _, ok := bucketMap[mapKey]; ok {
 		delete(bucketMap, mapKey)
 		return true
@@ -82,6 +121,13 @@ func (b *Bucket) removeItemFromDict(mapKey string, bucketMap map[string]string) 
 }
 
 func (b *Bucket) Dispatch() {
+	/*
+	Public method that will call all the private methods in the correct order.
+
+	Will check that key contains either "suspended" or "disabled" to perform the bucket versioning.
+
+	:return: nil
+	*/
 	b.bucketList()
 
 	if len(b.BucketList) == 0 {
@@ -95,7 +141,7 @@ func (b *Bucket) Dispatch() {
 
 		if strings.Contains(status, "suspended") || strings.Contains(status, "disabled") {
 			if b.removeItemFromDict(status, copyMap) {
-				b.updateBucketVersion(bucket, status)
+				b.updateBucketVersion(bucket)
 				log.Printf("Enabled versioning on bucket %v\n", bucket)
 
 				bucketMap = copyMap
