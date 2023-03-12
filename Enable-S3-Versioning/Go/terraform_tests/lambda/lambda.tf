acff14c9b68307b74e16d904b906b5291b8e2aaf
Anon4Now <tutkoda@gmail.com>
2023-03-12 15:51:02 -0400

    removed absolute path to zip

--- Enable-S3-Versioning/Go/terraform_tests/lambda/lambda.tf
+++ Enable-S3-Versioning/Go/terraform_tests/lambda/lambda.tf
@@ -8,7 +8,7 @@ variable "lambda_role_arn" {
 
 resource "aws_lambda_function" "versioning_lambda" {
   function_name = var.lambda_func_name
-  filename = "C:\\terraform_projects\\s3-enable-versioning-infra\\lambda\\main.zip"
+  filename = "PATH_TO_ZIP"
   role = var.lambda_role_arn
   handler = "main"
   runtime = "go1.x"
