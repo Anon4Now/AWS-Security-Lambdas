2e4a4dd6beda6cfe0b3fa79b43e01d68cdc52be2
Anon4Now <tutkoda@gmail.com>
2023-04-05 13:14:47 -0400

    updated terraform config to use lambda handler

--- Enable-CMK-Rotation-Yearly/Python/terraform_tests/lambda/lambda.tf
+++ Enable-CMK-Rotation-Yearly/Python/terraform_tests/lambda/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "versioning_lambda" {
   function_name = var.lambda_func_name
   filename = "./main.zip"
   role = var.lambda_role_arn
-  handler = "cmk_rotate_keys_one_year"
+  handler = "cmk_rotate_keys_one_year.lambda_handler"
   runtime = "python3.9"
 
 }
